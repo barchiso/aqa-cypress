b4f1df8359d73ca468c151fbe139e3a3c8a71fb8
Bohdan Lutsyk <xnunrgx@gmail.com>
2025-01-09 05:00:06 +0200

    Add 2 different configuration files. Add a script to package.json for each of the configurations.

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,24 @@
 import { defineConfig } from 'cypress';
+
 import 'cypress-mochawesome-reporter/plugin.js';
+import qautoConfig from './cypress/config/qauto-config.js';
+import qauto2Config from './cypress/config/qauto2-config.js';
+
+const environment = process.env.CYPRESS_ENV || 'qauto'; // Default to 'qauto' if no environment variable is set
+const config = environment === 'qauto2' ? qauto2Config : qautoConfig;
 
 export default defineConfig({
+	env: {
+		qauto: qautoConfig,
+		qauto2: qauto2Config,
+	},
 	reporter: 'cypress-mochawesome-reporter',
 	reporterOptions: {
 		reportDir: 'cypress/reports',
 		overwrite: true,
-		html: true,
-		json: true,
 	},
 	e2e: {
-		baseUrl: 'https://guest:welcome2qauto@qauto.forstudy.space/',
+		baseUrl: config.baseUrl, // Use the selected config's baseUrl
 		fixturesFolder: 'cypress/fixtures',
 		specPattern: '**/*.cy.{js,jsx,ts,tsx}',
 		screenshotOnRunFailure: true,
---
+++ cypress/config/qauto-config.js
@@ -0,0 +1,5 @@
+export default {
+	baseUrl: 'https://guest:welcome2qauto@qauto.forstudy.space/',
+	uniqueEmail: 'bohdan.lutsyk86+TestUser10@gmail.com',
+	uniquePassword: 'Password1!',
+};
